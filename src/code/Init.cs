0e59d7cad83edca7a00b88ce20c67c4150f1b728
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-04-05 16:09:26 -0500

    Change AnyPackage to 0.5.0

--- src/code/Init.cs
+++ src/code/Init.cs
@@ -2,6 +2,7 @@
 // You may use, distribute and modify this code under the
 // terms of the MIT license.
 
+using System;
 using System.Management.Automation;
 using static AnyPackage.Provider.PackageProviderManager;
 
@@ -9,14 +10,16 @@ namespace AnyPackage.Provider.Programs
 {
     public sealed class Init : IModuleAssemblyInitializer, IModuleAssemblyCleanup
     {
+        private readonly Guid _id = new Guid("4100e661-4a03-4e2a-855a-b9d17ed18b46");
+        
         public void OnImport()
         {
-            RegisterProvider(typeof(ProgramsProvider), "AnyPackage.Programs");
+            RegisterProvider(_id, typeof(ProgramsProvider), "AnyPackage.Programs");
         }
 
         public void OnRemove(PSModuleInfo psModuleInfo)
         {
-            UnregisterProvider(typeof(ProgramsProvider));
+            UnregisterProvider(_id);
         }
     }
 }
